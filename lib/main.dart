2488522b878ce672da64ffe41ff244b003af1066
hager khaled <hagerk720@gmail.com>
2022-03-28 14:14:29 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class TasksApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       
     );
   }
