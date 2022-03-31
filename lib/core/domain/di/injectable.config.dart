789dd6233898f9eafddbf0b63f870c2291d5bd1d
hager khaled <hagerk720@gmail.com>
2022-03-31 13:46:12 +0200

    logic of delete

--- lib/core/domain/di/injectable.config.dart
+++ lib/core/domain/di/injectable.config.dart
@@ -11,51 +11,59 @@ import 'package:shared_preferences/shared_preferences.dart' as _i5;
 
 import '../../../features/auth/data/datasources/auth_service.dart' as _i9;
 import '../../../features/auth/data/repositories/auth_repository_impl.dart'
-    as _i18;
+    as _i19;
 import '../../../features/auth/domain/repositories/auth_repository.dart'
-    as _i17;
-import '../../../features/auth/domain/usecases/login.dart' as _i23;
-import '../../../features/auth/domain/usecases/logout.dart' as _i24;
-import '../../../features/auth/domain/usecases/register.dart' as _i25;
-import '../../../features/auth/presentation/bloc/auth_cubit.dart' as _i27;
+    as _i18;
+import '../../../features/auth/domain/usecases/login.dart' as _i27;
+import '../../../features/auth/domain/usecases/logout.dart' as _i28;
+import '../../../features/auth/domain/usecases/register.dart' as _i29;
+import '../../../features/auth/presentation/bloc/auth_cubit.dart' as _i31;
+import '../../../features/delete_task/data/datasourses/delete_services.dart'
+    as _i13;
+import '../../../features/delete_task/data/repostories/delete_task_repository_imp.dart'
+    as _i23;
+import '../../../features/delete_task/domain/repositories/delete_task_repository.dart'
+    as _i22;
+import '../../../features/delete_task/domain/usecases/delete_task_usecase.dart'
+    as _i24;
 import '../../../features/get_tasks/data/datasources/get_tasks_service.dart'
     as _i4;
 import '../../../features/get_tasks/data/repositories/get_tasks_repository_impl.dart'
-    as _i22;
+    as _i26;
 import '../../../features/get_tasks/domain/repositories/get_tasks_repository.dart'
-    as _i21;
-import '../../../features/get_tasks/domain/usecases/get_tasks.dart' as _i29;
+    as _i25;
+import '../../../features/get_tasks/domain/usecases/get_tasks.dart' as _i33;
 import '../../../features/get_tasks/presentation/bloc/get_tasks_cubit.dart'
-    as _i30;
-import '../../../features/upload_task/core/bloc/upload_task_cubit.dart' as _i31;
+    as _i34;
+import '../../../features/upload_task/core/bloc/upload_task_cubit.dart' as _i35;
 import '../../../features/upload_task/create_task/data/datasources/create_task_api_service.dart'
     as _i10;
 import '../../../features/upload_task/create_task/data/datasources/create_task_service_impl.dart'
     as _i12;
 import '../../../features/upload_task/create_task/data/repositories/create_task_repository_impl.dart'
-    as _i20;
+    as _i21;
 import '../../../features/upload_task/create_task/domain/datasources/create_task_service.dart'
     as _i11;
 import '../../../features/upload_task/create_task/domain/repositories/create_task_repository.dart'
-    as _i19;
+    as _i20;
 import '../../../features/upload_task/create_task/domain/usecases/create_task.dart'
-    as _i28;
+    as _i32;
 import '../../../features/upload_task/update_task/data/datasources/update_task_api_service.dart'
     as _i6;
 import '../../../features/upload_task/update_task/data/datasources/update_task_service_impl.dart'
     as _i8;
 import '../../../features/upload_task/update_task/data/repositories/update_task_repositories_impl.dart'
-    as _i16;
+    as _i17;
 import '../../../features/upload_task/update_task/domain/datasources/update_task_service.dart'
     as _i7;
 import '../../../features/upload_task/update_task/domain/repositories/update_task_repositories.dart'
-    as _i15;
+    as _i16;
 import '../../../features/upload_task/update_task/domain/usecases/update_task.dart'
-    as _i26;
-import '../../data/datasources/local_datasource_impl.dart' as _i14;
-import '../datasources/local_datasource.dart' as _i13;
-import 'app_dio.dart' as _i32;
-import 'shared_pref.dart' as _i33; // ignore_for_file: unnecessary_lambdas
+    as _i30;
+import '../../data/datasources/local_datasource_impl.dart' as _i15;
+import '../datasources/local_datasource.dart' as _i14;
+import 'app_dio.dart' as _i36;
+import 'shared_pref.dart' as _i37; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -79,36 +87,43 @@ Future<_i1.GetIt> $initGetIt(_i1.GetIt get,
       () => _i10.CreateTaskApiService(get<_i3.Dio>()));
   gh.lazySingleton<_i11.CreateTaskService>(
       () => _i12.CreateTaskServiceImpl(get<_i10.CreateTaskApiService>()));
-  gh.lazySingleton<_i13.LocalDataSource>(
-      () => _i14.LocalDataSourceImpl(get<_i5.SharedPreferences>()));
-  gh.factory<_i15.UpdateTaskRepository>(() => _i16.UpdateTaskRepositoryImpl(
-      get<_i7.UpdateTaskService>(), get<_i13.LocalDataSource>()));
-  gh.factory<_i17.AuthRepository>(() => _i18.AuthRepositoryImpl(
+  gh.lazySingleton<_i13.DeleteTasksService>(
+      () => _i13.DeleteTasksService(get<_i3.Dio>()));
+  gh.lazySingleton<_i14.LocalDataSource>(
+      () => _i15.LocalDataSourceImpl(get<_i5.SharedPreferences>()));
+  gh.factory<_i16.UpdateTaskRepository>(() => _i17.UpdateTaskRepositoryImpl(
+      get<_i7.UpdateTaskService>(), get<_i14.LocalDataSource>()));
+  gh.factory<_i18.AuthRepository>(() => _i19.AuthRepositoryImpl(
       authService: get<_i9.AuthService>(),
-      localDataSource: get<_i13.LocalDataSource>()));
-  gh.factory<_i19.CreateTaskRepository>(() => _i20.CreateTaskRepositoryImpl(
-      get<_i11.CreateTaskService>(), get<_i13.LocalDataSource>()));
-  gh.factory<_i21.GetTasksRepository>(() => _i22.GetTasksRepositoryImpl(
+      localDataSource: get<_i14.LocalDataSource>()));
+  gh.factory<_i20.CreateTaskRepository>(() => _i21.CreateTaskRepositoryImpl(
+      get<_i11.CreateTaskService>(), get<_i14.LocalDataSource>()));
+  gh.factory<_i22.DeleteTaskRepository>(() => _i23.DeleteTaskRepositoryImpl(
+      deleteTaskService: get<_i13.DeleteTasksService>(),
+      localDataSource: get<_i14.LocalDataSource>()));
+  gh.factory<_i24.DeleteTaskUseCase>(
+      () => _i24.DeleteTaskUseCase(get<_i22.DeleteTaskRepository>()));
+  gh.factory<_i25.GetTasksRepository>(() => _i26.GetTasksRepositoryImpl(
       getTasksService: get<_i4.GetTasksService>(),
-      localDataSource: get<_i13.LocalDataSource>()));
-  gh.factory<_i23.Login>(() => _i23.Login(get<_i17.AuthRepository>()));
-  gh.factory<_i24.Logout>(() => _i24.Logout(get<_i17.AuthRepository>()));
-  gh.factory<_i25.Register>(() => _i25.Register(get<_i17.AuthRepository>()));
-  gh.factory<_i26.UpdateTask>(
-      () => _i26.UpdateTask(get<_i15.UpdateTaskRepository>()));
-  gh.factory<_i27.AuthCubit>(() => _i27.AuthCubit(
-      get<_i25.Register>(), get<_i23.Login>(), get<_i24.Logout>()));
-  gh.factory<_i28.CreateTask>(
-      () => _i28.CreateTask(get<_i19.CreateTaskRepository>()));
-  gh.factory<_i29.GetTasks>(
-      () => _i29.GetTasks(get<_i21.GetTasksRepository>()));
-  gh.factory<_i30.GetTasksCubit>(
-      () => _i30.GetTasksCubit(get<_i29.GetTasks>()));
-  gh.factory<_i31.UploadTaskCubit>(() =>
-      _i31.UploadTaskCubit(get<_i28.CreateTask>(), get<_i26.UpdateTask>()));
+      localDataSource: get<_i14.LocalDataSource>()));
+  gh.factory<_i27.Login>(() => _i27.Login(get<_i18.AuthRepository>()));
+  gh.factory<_i28.Logout>(() => _i28.Logout(get<_i18.AuthRepository>()));
+  gh.factory<_i29.Register>(() => _i29.Register(get<_i18.AuthRepository>()));
+  gh.factory<_i30.UpdateTask>(
+      () => _i30.UpdateTask(get<_i16.UpdateTaskRepository>()));
+  gh.factory<_i31.AuthCubit>(() => _i31.AuthCubit(
+      get<_i29.Register>(), get<_i27.Login>(), get<_i28.Logout>()));
+  gh.factory<_i32.CreateTask>(
+      () => _i32.CreateTask(get<_i20.CreateTaskRepository>()));
+  gh.factory<_i33.GetTasks>(
+      () => _i33.GetTasks(get<_i25.GetTasksRepository>()));
+  gh.factory<_i34.GetTasksCubit>(
+      () => _i34.GetTasksCubit(get<_i33.GetTasks>()));
+  gh.factory<_i35.UploadTaskCubit>(() =>
+      _i35.UploadTaskCubit(get<_i32.CreateTask>(), get<_i30.UpdateTask>()));
   return get;
 }
 
-class _$AppDio extends _i32.AppDio {}
+class _$AppDio extends _i36.AppDio {}
 
-class _$CacheHelper extends _i33.CacheHelper {}
+class _$CacheHelper extends _i37.CacheHelper {}
---
+++ lib/features/delete_task/domain/usecases/delete_task_usecase.dart
@@ -0,0 +1,34 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:injectable/injectable.dart';
+import 'package:tasks_app/core/domain/error/failure.dart';
+import 'package:tasks_app/core/domain/usecases/usecase.dart';
+import 'package:tasks_app/features/delete_task/domain/repositories/delete_task_repository.dart';
+
+@injectable
+class DeleteTaskUseCase implements UseCase<String,int> {
+  DeleteTaskRepository deleteTaskRepository;
+  DeleteTaskUseCase(this.deleteTaskRepository);
+
+  @override
+  Future<Either<Failure, String>> call(
+    int taskId,
+  ) {
+
+    return deleteTaskRepository.deleteTask(
+        taskId: taskId ,
+      );
+  }
+}
+
+class DeleteTaskData extends Equatable {
+  final int taskId;
+
+  const DeleteTaskData({
+    required this.taskId,
+  });
+  @override
+  List<Object?> get props => [
+        taskId,
+      ];
+}
